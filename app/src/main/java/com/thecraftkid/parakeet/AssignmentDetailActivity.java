6a45564d35973b41341ad150d3068d13cf74a4ff
Willie Chalmers III <willieechalmers@gmail.com>
2017-11-20 01:33:37 -0600

    Create AssignmentDetailActivity
    - Create AssignmentDetailFragment
    - Create corresponding LiveData and ViewModel

---
+++ app/src/main/java/com/thecraftkid/parakeet/AssignmentDetailActivity.java
@@ -0,0 +1,66 @@
+package com.thecraftkid.parakeet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.thecraftkid.parakeet.ui.AssignmentDetailFragment;
+
+import static com.thecraftkid.parakeet.util.IntentConstants.EXTRA_ASSIGNMENT_ID;
+import static com.thecraftkid.parakeet.util.IntentConstants.EXTRA_CLASSROOM_ID;
+import static java.util.Objects.requireNonNull;
+
+/**
+ * An {@link android.app.Activity} that displays details for a singular
+ * {@link com.thecraftkid.parakeet.model.Assignment}.
+ *
+ * @version 1.0.0
+ * @since v0.1.0 (11/19/2017)
+ */
+public class AssignmentDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignment_detail);
+        setSupportActionBar(findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent intent = getIntent();
+        String classId = requireNonNull(intent.getStringExtra(EXTRA_CLASSROOM_ID));
+        String assignmentId = requireNonNull(intent.getStringExtra(EXTRA_ASSIGNMENT_ID));
+
+        if (assignmentId != null) {
+            loadAssignment(classId, assignmentId);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_assignment_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_edit:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void loadAssignment(String classId, String assignmentId) {
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container,
+                        AssignmentDetailFragment.newInstance(classId, assignmentId))
+                .commit();
+    }
+
+    private void editAssignment() {
+        // TODO: 11/19/2017 Allow editing of assignments
+    }
+}
--- app/src/main/java/com/thecraftkid/parakeet/livedata/AssignmentLiveData.java
+++ app/src/main/java/com/thecraftkid/parakeet/livedata/AssignmentLiveData.java
@@ -4,10 +4,11 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.thecraftkid.parakeet.model.Assignment;
 
 /**
+ * An
+ *
  * @version 1.0.0
  * @since v1.0.0 (11/19/2017)
  */
-
 public class AssignmentLiveData extends SnapshotLiveData<Assignment> {
 
     public AssignmentLiveData(String classId, String assignmentId) {
