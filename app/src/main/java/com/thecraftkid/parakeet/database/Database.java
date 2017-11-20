61beef9005fd4686c78aca33a2eec481c28c49a8
Willie Chalmers III <willieechalmers@gmail.com>
2017-11-20 11:06:34 -0600

    Create database class as entrypoint for data

---
+++ app/src/main/java/com/thecraftkid/parakeet/database/Database.java
@@ -0,0 +1,13 @@
+package com.thecraftkid.parakeet.database;
+
+/**
+ * @version 1.0.0
+ * @since v1.0.0 (11/19/2017)
+ */
+
+public class Database {
+
+    public static void getAssignment(String classId, String assignmentId) {
+
+    }
+}
