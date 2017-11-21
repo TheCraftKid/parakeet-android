bf0debb212b402ee65afae3904a9510be9f10010
Willie Chalmers III <willieechalmers@gmail.com>
2017-11-21 13:32:34 -0600

    Update Assignment class to include grade

--- app/src/main/java/com/thecraftkid/parakeet/model/Assignment.kt
+++ app/src/main/java/com/thecraftkid/parakeet/model/Assignment.kt
@@ -4,4 +4,4 @@ package com.thecraftkid.parakeet.model
  * @since v1.0.0 (11/19/2017)
  * @version 1.0.0
  */
-class Assignment(name: String, notes: String?, dueDate: Long)
+class Assignment(name: String, notes: String?, dueDate: Long, totalPoints: Int, earnedPoints: Int)
