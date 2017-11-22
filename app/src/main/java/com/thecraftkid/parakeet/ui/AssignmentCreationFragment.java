d799cd18b3984203ff34f5cc8c62104501a45ec0
Willie Chalmers III <willieechalmers@gmail.com>
2017-11-22 10:15:12 -0600

    Add getters to AssignmentCreationViewModel

--- app/src/main/java/com/thecraftkid/parakeet/ui/AssignmentCreationFragment.java
+++ app/src/main/java/com/thecraftkid/parakeet/ui/AssignmentCreationFragment.java
@@ -1,10 +1,13 @@
 package com.thecraftkid.parakeet.ui;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,10 +23,12 @@ import com.thecraftkid.parakeet.R;
  */
 public class AssignmentCreationFragment extends Fragment {
 
+
     /**
      * Required public empty constructor
      */
     public AssignmentCreationFragment() {
+
     }
 
     @Override
@@ -34,10 +39,46 @@ public class AssignmentCreationFragment extends Fragment {
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-
+        AssignmentCreationViewModel viewModel = ViewModelProviders.of(this)
+                .get(AssignmentCreationViewModel.class);
     }
 
     private static class AssignmentCreationViewModel extends ViewModel {
-        private long dueDate;
+
+        private LiveData<String> name;
+
+        private LiveData<String> notes;
+
+        private LiveData<Long> dueDate;
+
+        private LiveData<Integer> totalPoints;
+
+        public LiveData<String> getName() {
+            if (name == null) {
+                name = new MutableLiveData<>();
+            }
+            return name;
+        }
+
+        public LiveData<String> getNotes() {
+            if (notes == null) {
+                notes = new MutableLiveData<>();
+            }
+            return notes;
+        }
+
+        public LiveData<Long> getDueDate() {
+            if (dueDate == null) {
+                dueDate = new MutableLiveData<>();
+            }
+            return dueDate;
+        }
+
+        public LiveData<Integer> getTotalPoints() {
+            if (totalPoints == null) {
+                totalPoints = new MutableLiveData<>();
+            }
+            return totalPoints;
+        }
     }
 }
