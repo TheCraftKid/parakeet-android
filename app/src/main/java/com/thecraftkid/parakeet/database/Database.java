fb67b9245a9e191c7a2fbf8e75802e6047aafeb7
Willie Chalmers III <willieechalmers@gmail.com>
2017-11-22 14:21:25 -0600

    Revise Database class structure

--- app/src/main/java/com/thecraftkid/parakeet/database/Database.java
+++ app/src/main/java/com/thecraftkid/parakeet/database/Database.java
@@ -1,13 +1,46 @@
 package com.thecraftkid.parakeet.database;
 
+import android.support.annotation.NonNull;
+
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.thecraftkid.parakeet.model.Assignment;
+
 /**
+ * A class that manages upload and download of data from the backend server.
+ *
  * @version 1.0.0
  * @since v1.0.0 (11/19/2017)
  */
-
 public class Database {
 
-    public static void getAssignment(String classId, String assignmentId) {
+    private static final String PATH_CLASSES = "classes";
 
+    private static final String PATH_ASSIGNMENTS = "assignments";
+
+    private static Database INSTANCE;
+
+    private FirebaseFirestore mDatabase;
+
+    private Database() {
+        mDatabase = FirebaseFirestore.getInstance();
+    }
+
+    public static Database getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new Database();
+        }
+        return INSTANCE;
+    }
+
+    @NonNull
+    private DocumentReference getClassReference(String classId) {
+        return mDatabase.collection(PATH_CLASSES).document(classId);
+    }
+
+    public Task<Void> upload(String classId, Assignment assignment) {
+        return getClassReference(classId).collection(PATH_ASSIGNMENTS)
+                .document().set(assignment);
     }
 }
