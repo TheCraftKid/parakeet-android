94225bbdd4e30197265d4a051168b74715a65d4d
Willie Chalmers III <willieechalmers@gmail.com>
2017-11-24 12:34:32 -0600

    Add getUserId utility method to UserManager

--- app/src/main/java/com/thecraftkid/parakeet/auth/UserManager.java
+++ app/src/main/java/com/thecraftkid/parakeet/auth/UserManager.java
@@ -2,6 +2,7 @@ package com.thecraftkid.parakeet.auth;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 
@@ -79,5 +80,13 @@ public final class UserManager {
                     Log.w(TAG, "Couldn't sign out", task.getException());
                     return task;
                 });
+    }
+
+    /**
+     * Returns the currently signed in user's unique identifier if one exists; null otherwise.
+     */
+    @Nullable
+    public String getUserId() {
+        return mAuth.getCurrentUser() != null ? mAuth.getCurrentUser().getUid() : null;
     }
 }
